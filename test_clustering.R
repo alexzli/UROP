be35d0a6e9bfd08e1cd1ded5b6d822f4c11f6a93
Alex Li <alexzli@mit.edu>
2022-01-21 18:00:03 -0500

    add t-SNE cluster visualization

--- test_clustering.R
+++ test_clustering.R
@@ -1,6 +1,6 @@
 library(Seurat)
 
-puck <- readRDS('../puckCropped.rds')
+puck <- readRDS('../UROP/data/puckCropped_cerebellum_slideseq.rds')
 slide.seq <- CreateSeuratObject(counts = puck@counts)
 
 slide.seq <- NormalizeData(slide.seq)
@@ -9,8 +9,9 @@ slide.seq <- ScaleData(slide.seq)
 #slide.seq <- SCTransform(slide.seq, ncells = 3000, verbose = FALSE)
 
 slide.seq <- RunPCA(slide.seq)
+#slide.seq <- RunTSNE(slide.seq, dims = 1:30)
 slide.seq <- RunUMAP(slide.seq, dims = 1:30)
 slide.seq <- FindNeighbors(slide.seq, dims = 1:30)
-slide.seq <- FindClusters(slide.seq, resolution = 0.3, verbose = FALSE)
-plot1 <- DimPlot(slide.seq, reduction = "umap", label = TRUE)
+slide.seq.clusters <- FindClusters(slide.seq, resolution = 0.3, verbose = FALSE)
+plot1 <- DimPlot(slide.seq.clusters, reduction = "umap", label = TRUE)
 plot1
