705f0968e3174ab53c07b16ba600646cc76b2638
Alex Li <alexzli@mit.edu>
2022-01-31 18:25:00 -0500

    update ru script

--- gen_sim_puck_script.R
+++ gen_sim_puck_script.R
@@ -2,10 +2,10 @@ datadir <- '/broad/thechenlab/Alex/UROP/'
 .libPaths( c( .libPaths(), file.path(datadir, 'Rpcg')))
 library(spacexr)
 library(Matrix)
-source('../spacexr/AnalysisCSIDE/helper_functions/de_simulation_helper.R')
+source('/broad/thechenlab/Alex/spacexr/AnalysisCSIDE/helper_functions/de_simulation_helper.R')
 
-reference <- readRDS('data/reference_RCTD_vec.rds')
+reference <- readRDS(file.path(datadir, 'data/reference_RCTD_vec.rds'))
 cell_types <- c('Astrocytes', 'Bergmann', 'Fibroblast', 'Golgi', 'Granule', 'MLI1', 'MLI2', 'Oligodendrocytes', 'Polydendrocytes', 'Purkinje')
 
 puck <- generate_sim_puck(cell_types, rownames(reference@counts), reference, trials=30)
-saveRDS(puck, 'data/sim_puck.rds')
+saveRDS(puck, file.path(datadir, 'data/sim_puck.rds'))
