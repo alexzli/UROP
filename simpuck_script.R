a2383ba86b69f5e8112ccb744de83bebfb48d700
Alex Li <alexzli@mit.edu>
2022-02-02 19:19:02 -0500

    run algorithm on simulated puck

--- simpuck_script.R
+++ simpuck_script.R
@@ -1,18 +1,9 @@
 datadir <- '/broad/thechenlab/Alex/UROP/'
 .libPaths( c( .libPaths(), file.path(datadir, 'Rpcg')))
 library(spacexr)
-library(Matrix)
-source(file.path(datadir, '/R/optimization.R'))
-source(file.path(datadir, '/R/initialization.R'))
-source(file.path(datadir, '/R/clustering.R'))
-source(file.path(datadir, '/R/analysis.R'))
-
+source(file.path(datadir, '/R/ITERATIVE_OPTIMIZATION.R')
 
 puck <- readRDS(file.path(datadir, '/data/sim_puck.rds'))
-assignments <- gen.clusters(puck, resolution = 0.3, SCT = F)
-cell_type_info <- cell_type_info_from_assignments(puck, assignments)
-myRCTD <- create.RCTD.noref(puck, cell_type_info = cell_type_info)
-myRCTD@config$max_cores <- 32
+RCTD_list <- run.unsupervised(puck, max_cores = 8, info_type = 'de')
 
-RCTDlist <- run.iter.optim(myRCTD, cell_types_assigned = FALSE, max_n_iter = 50, CELL_MIN_INSTANCE = 0, constant_genes = TRUE)
-saveRDS(RCTDlist, file.path(datadir, '/data/sim_puck_results.rds'))
+saveRDS(RCTDlist, file.path(datadir, '/data/sim_puck_results_de_init.rds'))
