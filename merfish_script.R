b51afa1e248926a412858eac9247685d3ac1f6d0
Alex Li <alexzli@mit.edu>
2022-02-05 13:42:32 -0500

    run on simulated MERFISH dataset

--- merfish_script.R
+++ merfish_script.R
@@ -5,5 +5,5 @@ library(Matrix)
 source(file.path(datadir, '/R/ITERATIVE_OPTIMIZATION.R')
 
 puck <- readRDS(file.path(datadir, '/data/MERFISH_puck_20um2.rds'))
-RCTD_list <- run.unsupervised(puck, resolution = 0.18, info_type = 'mean', fit_genes = 'mean', gene_list = puck@counts@Dimnames[[1]])
+RCTD_list <- run.unsupervised(puck, max_cores = 8, resolution = 0.18, info_type = 'mean', fit_genes = 'mean', gene_list = puck@counts@Dimnames[[1]])
 saveRDS(RCTD_list,file.path(datadir, '/data/MERFISH20um2_results_mean_fit_fullgenes.rds'))
