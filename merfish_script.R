d3ef6518e2353facc67e0601271596ba165e1b7d
Alex Li <alexzli@mit.edu>
2022-02-07 12:17:33 -0500

    analyze SCT normalization

--- merfish_script.R
+++ merfish_script.R
@@ -1,9 +1,7 @@
 datadir <- '/broad/thechenlab/Alex/UROP/'
-.libPaths( c( .libPaths(), file.path(datadir, 'Rpcg')))
-library(spacexr)
-library(Matrix)
-source(file.path(datadir, '/R/ITERATIVE_OPTIMIZATION.R')
+.libPaths( c(file.path(datadir, 'Rpcg'), .libPaths()))
+source(file.path(datadir, '/R/ITERATIVE_OPTIMIZATION.R'))
 
 puck <- readRDS(file.path(datadir, '/data/MERFISH_puck_20um2.rds'))
-RCTD_list <- run.unsupervised(puck, max_cores = 8, resolution = 0.18, info_type = 'mean', fit_genes = 'mean', gene_list = puck@counts@Dimnames[[1]])
-saveRDS(RCTD_list,file.path(datadir, '/data/MERFISH20um2_results_mean_fit_fullgenes.rds'))
+RCTD_list <- run.unsupervised(puck, max_cores = 8, resolution = 0.18, SCT = T, info_type = 'mean', fit_genes = 'de', gene_list = puck@counts@Dimnames[[1]])
+saveRDS(RCTD_list,file.path(datadir, '/data/MERFISH20um2_results_de_fit_2_10_SCT.rds'))
