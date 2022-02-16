fa8e08800739914e98cc05acd5e8122bd9f7e55b
Alex Li <alexzli@mit.edu>
2022-02-16 00:29:14 -0500

    remove redundant files

--- analysis_MERFISH.R
+++
@@ -1,7 +0,0 @@
-library(STdeconvolve)
-source('../UROP/R/optimization.R')
-source('../UROP/R/initialization.R')
-source('../UROP/R/clustering.R')
-source('../UROP/R/analysis.R')
-
-simFN7 <- readRDS('../UROP/objects/simFN7_10um2')
--- analysis_sim_puck.R
+++
@@ -1,8 +0,0 @@
-source('../UROP/R/optimization.R')
-source('../UROP/R/initialization.R')
-source('../UROP/R/clustering.R')
-source('../UROP/R/analysis.R')
-source('../spacexr/AnalysisCSIDE/helper_functions/de_simulation_helper.R')
-
-reference <- readRDS('../UROP/objects/reference_RCTD_vec.rds')
-puck <- generate_sim_puck(levels(reference@cell_types), rownames(reference@counts), reference)
