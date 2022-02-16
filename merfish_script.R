5d01ea894804aa8571627ada49c7a33fac0dfca4
Alex Li <alexzli@mit.edu>
2022-02-16 00:30:47 -0500

    remove redundant files

--- merfish_script.R
+++
@@ -1,7 +0,0 @@
-datadir <- '/broad/thechenlab/Alex/UROP/'
-.libPaths( c(file.path(datadir, 'Rpcg'), .libPaths()))
-source(file.path(datadir, '/R/ITERATIVE_OPTIMIZATION.R'))
-
-puck <- readRDS(file.path(datadir, '/data/MERFISH_puck_20um2.rds'))
-RCTD_list <- run.unsupervised(puck, max_cores = 8, resolution = 0.08, SCT = T, info_type = 'mean', fit_genes = 'de', gene_list = puck@counts@Dimnames[[1]])
-saveRDS(RCTD_list,file.path(datadir, '/data/MERFISH20um2_results_de_fit_2_10_SCT.rds'))
--- run_iter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-source /broad/software/scripts/useuse
-reuse -q R-4.0
-cd /broad/thechenlab/Alex/UROP/
-set -e
-mkdir -p logs
-Rscript merfish_script.R > logs/iter.txt
