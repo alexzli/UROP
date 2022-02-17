696b22f2bf8f1a6974910a82731dfbe5af1edea6
Alex Li <alexzli@mit.edu>
2022-02-17 02:25:11 -0500

    generate new simulated datasets

--- scripts/run_iter.sh
+++ scripts/run_iter.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source /broad/software/scripts/useuse
 reuse -q R-4.0
-cd /broad/thechenlab/Alex/UROP/
+cd /broad/thechenlab/Alex/UROP/scripts
 set -e
 mkdir -p logs
 Rscript gen_simulated_script.R > logs/iter.txt
