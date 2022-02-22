3311513c6337feab970c8cbac414325450f3909f
Alex Li <alexzli@mit.edu>
2022-02-22 11:29:29 -0500

    run variable UMI simulated puck

--- scripts/run_simulated_script.R
+++ scripts/run_simulated_script.R
@@ -1,9 +1,9 @@
 datadir <- '/broad/thechenlab/Alex/UROP/'
 .libPaths( c( .libPaths(), file.path(datadir, 'Rpcg')))
 library(spacexr)
-source(file.path(datadir, '/R/ITERATIVE_OPTIMIZATION.R')
+source(file.path(datadir, '/R/algorithm.R'))
 
-puck <- readRDS(file.path(datadir, '/data/sim_puck.rds'))
-RCTD_list <- run.unsupervised(puck, max_cores = 8, info_type = 'de')
 
-saveRDS(RCTDlist, file.path(datadir, '/data/sim_puck_results_de_init.rds'))
+puck <- readRDS(file.path(datadir, 'data/sim_puck_variableUMI_small.rds'))
+RCTD_list <- run.unsupervised(puck, max_cores = 8, resolution = 0.4, SCT = F, silhouette_cutoff = 0, info_type = 'mean', fit_genes = 'de')
+saveRDS(RCTD_list, file.path(datadir, 'data/sim_puck_variableUMI_results_4res_SILHOUETTE.rds'))
